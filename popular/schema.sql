8f5cf77768dcac24c04e97faeae78e2e0ea99175
pedro vinicyus faustino alves <faustino.vinicyus@gmail.com>
2025-12-10 00:16:12 -0300

    create tables only if not exists

--- popular/schema.sql
+++ popular/schema.sql
@@ -1,7 +1,7 @@
-create database votoVivo;
-use votoVivo;
+CREATE DATABASE IF NOT EXISTS votovivo;
+USE votovivo;
 
-CREATE TABLE Deputado (
+CREATE TABLE IF NOT EXISTS Deputado (
     idDeputado      INT PRIMARY KEY,
     nomeCivil        VARCHAR(200) NOT NULL,
     cpf              VARCHAR(14)  UNIQUE,
@@ -15,28 +15,16 @@ CREATE TABLE Deputado (
     urlWebsite       VARCHAR(300)
 );
 
-
-
-select * from Deputado;
-
-CREATE TABLE RedeSocial (
+CREATE TABLE IF NOT EXISTS RedeSocial (
     idRede INT PRIMARY KEY AUTO_INCREMENT,
     idDeputado INT NOT NULL,
     linkRedeSocial VARCHAR(300) NOT NULL,
-
     FOREIGN KEY (idDeputado) REFERENCES Deputado(idDeputado)
 );
 
-
-select * from RedeSocial;
-
-
-CREATE TABLE Despesa (
-    
+CREATE TABLE IF NOT EXISTS Despesa (
     idDeputado INT NOT NULL,
     codDocumento INT NOT NULL,
-
- 
     ano INT,
     cnpjCpfFornecedor VARCHAR(20),
     codLote INT,
@@ -54,37 +42,19 @@ CREATE TABLE Despesa (
     valorGlosa DECIMAL(10,2),
     valorLiquido DECIMAL(10,2),
     
-    
     PRIMARY KEY (idDeputado, codDocumento),
-    
-   
     FOREIGN KEY (idDeputado) REFERENCES Deputado(idDeputado)
 );
 
-select * from despesa;
-
-
-
-
-
-
-
-
-CREATE TABLE Partido (
+CREATE TABLE IF NOT EXISTS Partido (
     idPartido INT PRIMARY KEY AUTO_INCREMENT,
     siglaPartido VARCHAR(300),
     uriPartido        VARCHAR(200),
     nome  VARCHAR(300) 
 );
 
-select * from  Partido;
-
-
-
-
-CREATE TABLE Gabinete (
+CREATE TABLE IF NOT EXISTS Gabinete (
     idGabinete INT PRIMARY KEY AUTO_INCREMENT,
-    
     andar      VARCHAR(10),
     emailGabinete VARCHAR(150),
     nomeGabinete  VARCHAR(200),
@@ -93,13 +63,7 @@ CREATE TABLE Gabinete (
     telefone      VARCHAR(20)
 );
 
-
-
-select * from gabinete;
-
-
-
-CREATE TABLE Historico (
+CREATE TABLE IF NOT EXISTS Historico (
     idHistorico INT PRIMARY KEY AUTO_INCREMENT, 
     idDeputado INT NOT NULL,
     idPartido INT NOT NULL,
@@ -116,13 +80,9 @@ CREATE TABLE Historico (
     nomeEleitoral VARCHAR(200),
     siglaUF CHAR(2),
     
-    
     UNIQUE KEY uk_historico (idDeputado, dataHistorico, situacao),
 
     FOREIGN KEY (idGabinete) REFERENCES Gabinete(idGabinete),
     FOREIGN KEY (idDeputado) REFERENCES Deputado(idDeputado),
     FOREIGN KEY (idPartido) REFERENCES Partido(idPartido)
 );
-
-select * from Historico;
-
